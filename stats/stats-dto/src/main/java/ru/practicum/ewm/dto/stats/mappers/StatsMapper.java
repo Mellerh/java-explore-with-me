278625f1ca2784311b8a06dbc40ff9be59d8b510
Tras V <103604450+Mellerh@users.noreply.github.com>
2025-02-12 23:48:15 +0300

    checkStyle fixes

--- stats/stats-dto/src/main/java/ru/practicum/ewm/dto/stats/mappers/StatsMapper.java
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/dto/stats/mappers/StatsMapper.java
@@ -1,12 +1,8 @@
 package ru.practicum.ewm.dto.stats.mappers;
 
-import jakarta.validation.ValidationException;
 import org.springframework.stereotype.Component;
 import ru.practicum.ewm.dto.stats.ViewStatsRequest;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
